e703d1a9f5f6da6099eacec8e16496eb4205b908
Alok sivam <97817601+Aloksivam@users.noreply.github.com>
2024-08-07 10:29:56 +0530

    Update DBconnection.java

--- src/jframe/DBconnection.java
+++ src/jframe/DBconnection.java
@@ -16,7 +16,7 @@ public class DBconnection {
     public static Connection getConnection(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","Al9ok@1435");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","*******");
         }
         catch(Exception e){
             e.printStackTrace();
